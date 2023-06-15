b9f4e8224a154f2e299142939cdb0fba7e843e9c
michael <michaelokuge742@gmail.com>
2023-06-15 14:43:23 -0700

    4-puts.c/5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 
 /**
  * main- starting point
- * return (0) - success
+ * return: Always 0 (success)
  */
 
-int main() 
+int main(void) 
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
