a44730e27f2d6895c27315c55cd60afbf98eb3e9
michael <michaelokuge742@gmail.com>
2023-06-16 14:29:12 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main- starting point
+ *
+ * Return : Always 0 (success)
+ */
+
+int main(void)
+{
+	char XX;
+	XX = a;
+
+	do 
+	{
+		putchar(XX);
+	}
+	while (XX <= z);
+		
+	return (0);
+}
