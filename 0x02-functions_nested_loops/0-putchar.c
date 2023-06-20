b9ba0c975ab3ede1921ee055f1c585a6b93ac214
michael <michaelokuge742@gmail.com>
2023-06-20 10:28:57 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print _putchar followed by newline
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	putchar("_putchar");
+	putchar("\n");
+	return (0);
+}
