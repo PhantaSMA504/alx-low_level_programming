9860dcd66a3c5f826cfeb67f423d8b63b128cd7e
michael <michaelokuge742@gmail.com>
2023-06-22 00:14:08 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main,h"
+
+/**
+ * int add - adds two integers and returns the result
+ * @n: character to be computed
+ *
+ * Return: Always 0 (success)
+ */
+
+int add(int a, int b);
+{
+	int result;
+
+	result = a + b;
+	_putchar(result + '0');
+	return (0);
+}
