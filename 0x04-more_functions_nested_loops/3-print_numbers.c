47f82bace52d8b71aa29a3ed9151b7cf2294a924
michael <michaelokuge742@gmail.com>
2023-06-23 05:59:29 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0-9
+ * followed by new line
+ *
+ * Return: Always 0 (success)
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+	return(0);
+}
