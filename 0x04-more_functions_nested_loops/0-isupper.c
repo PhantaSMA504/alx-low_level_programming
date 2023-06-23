dccbc088688f0154dff2bd8cd19e793028309abd
michael <michaelokuge742@gmail.com>
2023-06-23 03:06:56 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -10,16 +10,13 @@
 
 int _isupper(int c)
 {
-	for (c = 'A'; c <= 'Z'; c++)
+	if (c >= 'A' && c <= 'Z')
 	{
-		if (!(c >= 'A' && c <= 'Z'))
-		{
-			return (0);
-		}
-		else
-		{
-			return (1);
-		}
+		return (1);
+	}
+	else
+	{
+		return (0);
 	}
 	return(0);
 }
