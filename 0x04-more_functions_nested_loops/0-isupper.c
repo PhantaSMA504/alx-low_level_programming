c67bee287b41f1af20a284f08ed1d2e52204402b
michael <michaelokuge742@gmail.com>
2023-06-23 03:09:18 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * _isupper: checks for uppercase characters
+ * _isupper - checks for uppercase characters
  * @c: character to be investigated
- *
  * Return: 1 if c is uppercase
  * 0 if otherwise
  */
