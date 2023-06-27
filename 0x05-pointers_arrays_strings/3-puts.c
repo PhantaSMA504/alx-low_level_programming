dba3da576ab253776787929f385904e0c4dcbf4b
michael <michaelokuge742@gmail.com>
2023-06-27 14:25:40 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - prints out a string
+ * followed by a newline
+ *
+ * Return: Always 0 (success)
+ */
+
+void _puts(char *str)
+{
+	char *str[]= "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	_putchar(*str);
+	_putchar('\n');
+}
