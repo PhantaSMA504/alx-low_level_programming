c398553f4c7275943107fb8e79b21c03028f9eff
michael <michaelokuge742@gmail.com>
2023-06-27 14:38:27 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -9,7 +9,13 @@
 
 void _puts(char *str)
 {
-	str[] = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-	_putchar(str);
+	void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		++str;
+	}
 	_putchar('\n');
+}
 }
