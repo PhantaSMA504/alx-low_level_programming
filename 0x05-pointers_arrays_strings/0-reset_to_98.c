54d3db882ba0850c21eecc83d3bde30324da9081
michael <michaelokuge742@gmail.com>
2023-06-27 12:10:07 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * void reset_to_98 - resets integer value to 98
+ * @n: integer to be reset
+ * Return: Always 0 (success)
+ */
+
+void reset_to_98(int *n)
+{
+	int x;
+
+	x = 400
+	*n = &x
+	n = 98
+
+}
