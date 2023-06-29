9b208e041a0f9f08dc76bc137e07523dc7dfb8ca
michael <michaelokuge742@gmail.com>
2023-06-29 16:29:24 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,17 +2,34 @@
  * _strncat - concatenates two strings
  * @dest: string to be added to
  * @src: string to be added
+ * @n: bytes of @src
  *
  * Return: dest
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-	dest[] = "hello";
-	src[] = "world";
+	int i;
 
-	strncat( dest, src, 20);
-	_putchar(dest);
+	/**
+	 * iterate through src array 
+	 * where if there is no null byte
+	 * among the first n bytes
+	 * the string placed in n will not be terminated
+	 */
 
-	return (0);
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+		dest[i] = src[i];
+	/**
+	 * if the length of src is more than n
+	 * write additional n bytes to dest
+	 * to ensure a total of n bytesi is written
+	 */
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
 }
