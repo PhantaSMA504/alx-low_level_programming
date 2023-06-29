79c2a4d76cae397c2c03437812f5a110e16d281e
michael <michaelokuge742@gmail.com>
2023-06-29 15:49:56 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,18 @@
+/**
+ * _strncat - concatenates two strings
+ * @dest: string to be added to
+ * @src: string to be added
+ *
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	dest[] = "hello";
+	src[] = "world";
+
+	strncat( dest, src, 20);
+	_putchar( dest );
+
+	return (0);
+}
