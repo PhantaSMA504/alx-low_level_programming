ccc4b05579fade437a0165ca2031beeeb0f80270
michael <michaelokuge742@gmail.com>
2023-07-12 05:22:11 -0700

    It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints ot name of function
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0 (success)
+ */
+
+int main( __attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return(0);
+}
